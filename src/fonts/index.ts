2906f8f6f2ae777bba1a87287fdf702c8945f422
mauLovera <mauricioloveraedu@gmail.com>
2024-03-31 08:14:08 -0400

    feat: init

---
+++ src/fonts/index.ts
@@ -0,0 +1,4 @@
+import { Fraunces, Montserrat } from "next/font/google"
+
+export const sans = Montserrat({ weight: ["500", "700"], subsets: ["latin"] })
+export const serif = Fraunces({ weight: ["700"], subsets: ["latin"] })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        "cream": "hsl(30, 38%, 92%)",
       },
-    },
+      neutral: {
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+        "white": "(0, 0%, 100%)",
+      }
+    }
   },
   plugins: [],
 };
